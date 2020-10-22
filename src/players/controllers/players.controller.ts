32a80e3f27a3812167c2d4689e9f88413ac85aa4
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-21 23:12:03 -0300

    feat: add crud players

--- src/players/controllers/players.controller.ts
+++ src/players/controllers/players.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CreatePlayerDto } from '../dtos/create-player.dto';
+import { Player } from '../models/player.model';
 import { PlayersService } from '../services/players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
 
   constructor(private readonly playersService: PlayersService) {}
+
+  @Get()
+  async findAll(): Promise<Player[]> {
+    return this.playersService.findAll();
+  }
+
+  @Get(':email')
+  async findByEmail(@Param('email') email: string): Promise<Player> {
+    return this.playersService.findByEmail(email);
+  }
   
   @Post()
   async createOrUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
     return this.playersService.createOrUpdatePlayer(createPlayerDto);
+  }
+
+  @Delete(':email')
+  async delete(@Param('email') email: string): Promise<void> {
+    return this.playersService.deleteByEmail(email);
   }
 }
--- src/players/services/players.service.ts
+++ src/players/services/players.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePlayerDto } from '../dtos/create-player.dto';
 import { Player } from '../models/player.model';
 
@@ -9,7 +9,39 @@ export class PlayersService {
   private players: Player[] = [];
 
   async createOrUpdatePlayer(createPlayerDto: CreatePlayerDto): Promise<void> {
-    this.create(createPlayerDto);
+    const {email, name} = createPlayerDto;
+
+    const findPlayer = this.players.find(p => p.email === email);
+
+    if (!findPlayer) {
+      this.create(createPlayerDto);
+    } else  {
+      this.update(findPlayer._id, name);
+    }
+  }
+
+  async findAll(): Promise<Player[]> {
+    return this.players;
+  }
+
+  async findByEmail(email: string): Promise<Player> {
+    const player = this.players.find(p => p.email === email);
+
+    if(!player) {
+      throw new NotFoundException('Player not found')
+    }
+
+    return player;
+  }
+
+  async deleteByEmail(email: string) {
+    const player = this.players.find(p => p.email === email);
+
+    if(!player) {
+      throw new NotFoundException('Player not found')
+    }
+
+    this.players = this.players.filter(p => p._id !== player._id);
   }
 
   create(createPlayerDto: CreatePlayerDto): void {
@@ -22,5 +54,15 @@ export class PlayersService {
     }
 
     this.players.push(player);
+  }
+
+  update(id: string, name: string) {
+    this.players = this.players.map(player => {
+      if(player._id === id) {
+        player.name = name;
+      }
+
+      return player;
+    })
   }
 }
