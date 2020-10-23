4057d370b5568803939963363bba5453b3dc7acb
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-22 22:29:02 -0300

    chore: add mongoose module and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    }),
+    PlayersModule
+  ],
   controllers: [],
   providers: [],
 })
