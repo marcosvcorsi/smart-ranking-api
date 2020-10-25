bdef73dc463296301270a07fe64cbbd8acd7e3a6
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-24 22:14:59 -0300

    test: add create service tests

--- src/players/services/players.service.spec.ts
+++ src/players/services/players.service.spec.ts
@@ -1,12 +1,17 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreatePlayerDto } from '../dtos/create-player.dto';
+import { UpdatePlayerDto } from '../dtos/update-player.dto';
+import { Player } from '../models/player.schema';
 import { PlayersRepository } from '../repositories/players.repository';
 import { PlayersService } from './players.service';
 
 describe('PlayersService', () => {
   let service: PlayersService;
+  let repository: PlayersRepository;
   let mockCreatePlayer: CreatePlayerDto;
-
+  let mockUpdatePlayer: UpdatePlayerDto;
+  let mockPlayer: Player;
 
   beforeEach(async () => {
     const mockRepository = {
@@ -19,11 +24,23 @@ describe('PlayersService', () => {
     }).compile();
 
     service = module.get<PlayersService>(PlayersService);
+    repository = module.get<PlayersRepository>(PlayersRepository);
 
-    mockCreatePlayer = {
-      email: 'anymail@mail.com',
+    mockUpdatePlayer = {
       name: 'anyname',
       phoneNumber: '0000000'
+    }
+
+    mockCreatePlayer = {
+      ...mockUpdatePlayer,
+      email: 'anymail@mail.com',
+    }
+
+    mockPlayer = {
+      ...mockCreatePlayer,
+      ranking: 'a',
+      rankingPosition: 1,
+      imgUrl: 'any'
     }
   });
 
@@ -33,7 +50,7 @@ describe('PlayersService', () => {
 
   describe('create()', () => {
     it('should call create with correct values', async () => {
-      const createSpy = jest.spyOn(service, 'create');
+      const createSpy = jest.spyOn(repository, 'create');
 
       await service.create(mockCreatePlayer);
 
@@ -41,9 +58,23 @@ describe('PlayersService', () => {
     })
 
     it('should throw if create throws', async () => {
-      jest.spyOn(service, 'create').mockRejectedValueOnce(new Error())
+      jest.spyOn(repository, 'create').mockRejectedValueOnce(new Error())
 
       await expect(service.create(mockCreatePlayer)).rejects.toThrow(new Error());
+    })
+
+    it('should return an error if email already exists', async () => {
+      jest.spyOn(repository, 'findByEmail').mockReturnValueOnce(Promise.resolve(mockPlayer))
+
+      await expect(service.create(mockCreatePlayer)).rejects.toBeInstanceOf(BadRequestException);
+    })
+
+    it('should return a player on success', async () => {
+      jest.spyOn(repository, 'create').mockReturnValueOnce(Promise.resolve(mockPlayer))
+
+      const response = await service.create(mockCreatePlayer);
+
+      expect(response).toEqual(mockPlayer);
     })
   })
 });
