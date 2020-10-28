676e1341532d109ee4bdd6a4d1933fba13f134b2
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-27 23:12:52 -0300

    feat: add all exceptions filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './shared/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
   await app.listen(3333);
 }
 bootstrap();
